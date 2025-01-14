19d5d1b895c28fc96ab3dbf98fb6ab8633aac3f7
nathnael getachew <nathnaelgetachew12f@gmail.com>
2025-01-14 13:07:32 +0300

    added resume data

---
+++ src/data/resume.tsx
@@ -0,0 +1,440 @@
+import { Icons } from "@/components/icons";
+import { CodeIcon, HomeIcon, PencilLine } from "lucide-react";
+
+export const DATA = {
+  name: "Nathnael Getachew",
+  initials: "NG",
+  url: "nathnael-getachew-senishaw.netlify.app",
+  location: "Addis Ababa, Ethiopia",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer and fronted engineer with a passion of bulding beautiful, funcional websites.",
+  summary:
+    "#",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Go",
+    "HTML",
+    "CSS",
+    "Tailwind CSS",
+    "Javascript",
+    "mySQL",
+    "PhP"
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "#", icon: CodeIcon, label: "Projects" },
+    { href: "#", icon: PencilLine, label: "Notes" },
+  ],
+  contact: {
+    email: "nathnaelgetachew118@gmail.com",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "#",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "#",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+        company: "#",
+        href: "#",
+        badges: [],
+        location: "Remote",
+        title: "#",
+        logoUrl: "/img.png",
+        start: "May 2021",
+        end: "Oct 2022",
+        description:
+          "#",
+      },{
+        company: "#",
+        href: "#",
+        badges: [],
+        location: "Remote",
+        title: "#",
+        logoUrl: "/img.png",
+        start: "May 2021",
+        end: "Oct 2022",
+        description:
+          "#",
+      },{
+        company: "#",
+        href: "#",
+        badges: [],
+        location: "Remote",
+        title: "#",
+        logoUrl: "/img.png",
+        start: "May 2021",
+        end: "Oct 2022",
+        description:
+          "#",
+      },{
+        company: "#",
+        href: "#",
+        badges: [],
+        location: "Remote",
+        title: "#",
+        logoUrl: "/img.png",
+        start: "May 2021",
+        end: "Oct 2022",
+        description:
+          "#",
+      },
+  ],
+  education: [
+    {
+      school: "Addis Ababa University, AAiT",
+      href: "addisababauniversity.com",
+      degree: "bachlors degree in software engineering(Information Technology Stream)",
+      logoUrl: "/img.jpg",
+      start: "2020",
+      end: "2025",
+    },
+    {
+        school: "#",
+        href: "#",
+        degree: "#",
+        logoUrl: "/img.png",
+        start: "#",
+        end: "#",
+      },{
+        school: "#",
+        href: "#",
+        degree: "#",
+        logoUrl: "/img.png",
+        start: "#",
+        end: "#",
+      },{
+        school: "#",
+        href: "#",
+        degree: "#",
+        logoUrl: "/img.png",
+        start: "#",
+        end: "#",
+      },
+  ],
+  projects: [
+    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },    {
+        title: "#",
+        href: "#",
+        dates: "Jan  - Feb ",
+        active: true,
+        description:
+          "#",
+        technologies: [
+          "Next.js",
+          "Typescript",
+          "PostgreSQL",
+          "Prisma",
+          "TailwindCSS",
+          "Stripe",
+          "Shadcn UI",
+          "Magic UI",
+        ],
+        links: [
+          {
+            type: "Website",
+            href: "#",
+            icon: <Icons.globe className="size-3" />,
+          },
+        ],
+        image: "",
+        video:
+          "#",
+      },
+  ],
+} as const;
